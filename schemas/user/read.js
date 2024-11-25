aabeec928193cd33de60090dce228d0aa5faec0d
Javierdrw <javier.jg359@gmail.com>
2024-11-25 00:20:09 -0500

    ajustando cierta validacion

--- schemas/user/read.js
+++ schemas/user/read.js
@@ -1,7 +1,7 @@
 import joi from "joi-oid";
 
 export const schemaReadUser = joi.object({
-    _id: joi.objectId().required().messages({
+    id: joi.objectId().required().messages({
         "objectid.base": "Property must be a valid ObjectId",
         "any.required": "Property is required",
     }),
